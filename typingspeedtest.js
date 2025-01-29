937e37da5a73a9b0b6a7e79a36d3d6e9f65573b6
zohaibhassan9 <zabaidafashion@gmail.com>
2025-01-29 22:15:19 +0500

    IDs html elemts in js.

--- typingspeedtest.js
+++ typingspeedtest.js
@@ -3,3 +3,9 @@ const texts = ['Daly sunshine is a pert of our lives.',
     'Useful for every being in the world.'
 
 ]
+
+
+const textInput = document.getElementById('textInput');
+const textDisplay = document.getElementById('textDisplay');
+const startBtn = document.getElementById('startBtn');
+const stats = document.getElementById('stats');
